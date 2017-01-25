fcd16e362c3c1bfc0a20ea0b1e19cb55c7b87876
hpackonu <h-pack@onu.edu>
2017-01-25 01:39:27 -0500

    Add files via upload
    Fancier Hello World

---
+++ Hello World Fancy/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>This is a title!</title>
+    </head>
+    <body>
+        <p id="test">
+            Hello World
+        </p>
+        <span id="second">
+            This is a different test
+        </span>
+        <span class=green>Why <span class="purple">not </span> have</span> <span class="purple"> more colors? </span>
+    </body>
+    <style type="text/css">
+        body
+        {
+        background-image:url('earth.jpg');
+        }
+    </style>
+</html>
