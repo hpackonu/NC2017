992ef16e2e77d9c948627944f2f118add146653a
hpackonu <h-pack@onu.edu>
2017-02-01 01:47:22 -0500

    Add files via upload
    CSS Assignment

---
+++ CSSAssignment/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css.css">
+        <title>CSS Fun Times</title>
+    </head>
+    <body>
+        <div id="body">
+            This is the main body of the webpage and should have fancy text and be centered to some extent.
+        </div>
+    </body>
+</html>
