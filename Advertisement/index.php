4d39dd872f7eaf83822800c06493387524db195d
hpackonu <h-pack@onu.edu>
2017-02-10 02:04:40 -0500

    Add files via upload
    Recreate advertisement using CSS

---
+++ Advertisement/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css">
+        <title>Geico Advertisement</title>
+    </head>
+    <body>
+        <div id="Border">
+            <div id="Body">
+                <div id="Header">
+                    Saving People Money Since 1936
+                </div>
+                <div id="Banner">
+                    <div class="Main" id="BannerText">
+                    ... that's before the modern treadmill.
+                    </div>
+                </div>
+                <div class="Main" id="Quote">
+                    GEICO has been serving up great car insurance and fantastic customer service for more than 75 years. Get a quote and see how much you could save today.
+                </div>
+                <div class="Main" id="Information">
+                    geico.com | 1-800-947-AUTO | local office
+                </div>
+                <div class="Main" id="Geico">
+                    GEICO<span id="tiny">®</span>
+                </div>
+                <div id="Treadmill">
+                    <img id="LargeHatImage" src="Treadmill.PNG" alt=""/>
+                </div>
+            </div>
+
+            <div id="Footer">
+                    Some discounts, coverages, payment plans and feature are not available in all states or all GEICO companies. GEICO is a registered service mark of Government Employees Insurance Company, Washington, D.C. 20076; a Berkshire Hathaway Inc. subsidiary. © 2015 GEICO
+            </div>
+        </div>
+    </body>
+</html>
